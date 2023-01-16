4dda26a9932b11de3e37eee9b6cc8e8ee18b8f07
Jake Steckler <jsteckler@hks.harvard.edu>
2023-01-16 10:24:48 -0500

    loop over members and write a row for each member

--- superheroes.py
+++ superheroes.py
@@ -21,3 +21,22 @@ with open("superheroes.csv", "w") as f:
             "active",
         ]
     )
+
+    #oop over the members, and for each member write a row to the csv file
+        for superhero in superheroes["members"]:
+        writer.writerow(
+            [
+                superhero["name"],
+                superhero["age"],
+                superhero["secretIdentity"],
+                superhero["powers"],
+                superheroes["squadName"],
+                superheroes["homeTown"],
+                superheroes["formed"],
+                superheroes["secretBase"],
+                superheroes["active"],
+            ]
+        )
+
+
+        
