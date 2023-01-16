062678e4bd18ca836a6c7f6ef00e2f95c81ad3c3
Jake Steckler <jsteckler@hks.harvard.edu>
2023-01-16 10:23:03 -0500

    output into csv file with a header

--- superheroes.py
+++ superheroes.py
@@ -4,3 +4,20 @@ import csv
 #Read superheroes.json
 with open('superheroes.json') as f:
 	superheroes = json.load(f)
+
+#Write a header to CSV file
+with open("superheroes.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(
+        [
+            "name",
+            "age",
+            "secretIdentity",
+            "powers",
+            "squadName",
+            "homeTown",
+            "formed",
+            "secretBase",
+            "active",
+        ]
+    )
